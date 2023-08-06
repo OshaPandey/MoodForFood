0adb027dd3b0a609ed006dac093a9050e99eb7eb
Osha Pandey <osha.pandey8@gmail.com>
2023-08-06 13:12:22 +0530

    latestt commit

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -8,12 +8,11 @@ export default function Signup() {
     password: '',
     geolocation: ''
   });
-
-  const [signupSuccess, setSignupSuccess] = useState(false); // New state for success message
+  const [signupSuccess, setSignupSuccess] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    console.log(JSON.stringify({name:credentials.name,email:credentials.email,password:credentials.password,location:credentials.geolocation}))
     const response = await fetch("https://moodforfood-backend.onrender.com/api/createuser", {
       method: 'POST',
       headers: {
@@ -41,17 +40,30 @@ export default function Signup() {
   };
       
   return (
+    <>
     <div className='container'>
-      <form onSubmit={handleSubmit}>
-        {signupSuccess && <p className="text-success">Signed up successfully!</p>} {/* Display success message */}
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" name="name" value={credentials.name} onChange={onChange}/>
-        </div>
-        {/* ... rest of your form fields ... */}
-        <button type="submit" className="m-3 btn btn-text-white bg-success">Submit</button>
-        <Link to="/login" className='m-3 btn btn-danger'>Already a user</Link>
-      </form>   
-    </div>
+     <form onSubmit={handleSubmit}>
+     <div className="mb-3">
+    <label htmlFor="name" className="form-label">Name</label>
+    <input type="text" className="form-control" name="name" value={credentials.name} onChange={onChange}/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" name='email' value={credentials.email}  onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" name='password' value={credentials.password}  onChange={onChange} id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Address</label>
+    <input type="text" className="form-control" name='geolocation' value={credentials.geolocation}  onChange={onChange} id="exampleInputPassword1"/>
+  </div>
+  <button type="submit" className="m-3 btn btn-text-white bg-success">Submit</button>
+  <Link to="/login" className='m-3 btn btn-danger'>Already a user</Link>
+</form>   
+</div>
+    </>
   )
 }
